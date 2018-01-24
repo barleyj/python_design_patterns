128475223d543b3ecb368a07bb08dfdbc30ac6ba
Jayson Barley <jayson.barley@puppet.com>
2018-01-24 14:22:52 -0800

    Add tests

---
+++ tests/test_singleton.py
@@ -0,0 +1,14 @@
+from design_patterns.singleton import SingletonMeta
+
+def test_singleton():
+    class Test():
+        __metaclass__=SingletonMeta
+
+        def __init__(self, value):
+            self.value = value
+
+
+    t1 = Test('t1')
+    t2 = Test('t2')
+
+    assert t1.value == t2.value
