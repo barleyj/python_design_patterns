c67e95fa42ab2e9847c046f403289c94643ada78
Jayson Barley <jayson.barley@puppet.com>
2018-01-27 17:30:26 -0800

    Add instance test

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -12,3 +12,17 @@ def test_singleton():
     t2 = Test('t2')
 
     assert t1.value == t2.value
+
+
+def test_instance():
+    class Test():
+        __metaclass__=SingletonMeta
+
+        def __init__(self, value):
+            self.value = value
+
+
+    t1 = Test('t1')
+    t2 = Test('t2')
+
+    assert t1.instance == t2.instance
